13b0a6b52fd0f2e4da07e1d47ea71a7db53a165b
Andreas <ajorge22@student.aau.dk>
2023-11-03 10:07:41 +0100

    Kommentarer til koden
    Kommentarer til koden

--- Assets/Andreas/PositionController.cs
+++ Assets/Andreas/PositionController.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class PositionController : MonoBehaviour
 {
-    public Transform cameraToFollow; // Reference to the camera's Transform component
+    //Reference to the camera's Transform component
+    public Transform cameraToFollow; 
 
     void Update()
     {
-        //if (cameraToFollow != null) // Check if the cameraToFollow reference is set
-        //{
-            // Calculate the direction from the GameObject to the camera
+            //lookDirection is a Vector3 variable that stores the direction from the camera's position to the object's position
+            //cameraToFollow is a variable representing the position of a camera in the scene
+            //transform.position is a reference to the position of the emojis
             Vector3 lookDirection = cameraToFollow.position - transform.position;
 
-            // Invert the look direction by multiplying it by -1
+            //Inverts the lookDirection vector. Multiplying a vector by -1 essentially flips its direction
             lookDirection *= -1;
-
-            // Create a rotation that faces the camera but keeps the same up direction
+            
+            /*Quaternion.LookRotation is a function that creates a rotation quaternion based on the specified direction.
+            It computes a rotation that aligns the forward direction with the given lookDirection*/
             Quaternion rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
 
-            // Apply the rotation to the GameObject
+            //sets the rotation of the object's transform to the rotation quaternion calculated. It makes the object face in the direction of the camera
             transform.rotation = rotation;
-        //}
+        
     }
 }
