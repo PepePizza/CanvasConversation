e150dd42d72c2916d505b61c067aeeb53947cf68
StureJ <FT00FN@student.aau.dk>
2023-11-15 10:59:32 +0100

    Comment Creator
    thingy

---
+++ Assets/Sture/CommentPlacement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CommentPlacement : MonoBehaviour
+{
+    [SerializeField, TextArea] private string customText;
+    [SerializeField] private Sprite commentImage;
+    [SerializeField] private TMP_Text commentText;
+    [SerializeField] private GameObject avatarPicture;
+    private string imageName;
+    
+    [SerializeField] private Transform commentHolder; // Reference to the comment holder in the Inspector
+    [SerializeField] private ScrollRect scrollRect;
+    
+    void Start()
+    {
+        // Check if the comment holder and ScrollRect are assigned
+        if (commentHolder != null && scrollRect != null)
+        {
+            transform.SetParent(commentHolder);
+            transform.localPosition = Vector3.zero;
+            
+            Canvas.ForceUpdateCanvases();
+            scrollRect.normalizedPosition = new Vector2(0, 1);
+            
+            imageName = commentImage.name.Split('.')[0];
+            imageName = char.ToUpper(imageName[0]) + imageName.Substring(1);
+
+            avatarPicture.GetComponent<Image>().sprite = commentImage;
+            
+            commentText.text = "<color=#6A6A6A><size=25>Anonymous " + imageName + "</size></color>\n" + customText;
+            
+        }
+        
+    }
+    
+    
+}
+
+
